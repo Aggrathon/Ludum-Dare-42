629b91b29a66f194f090d7aa295596ca3ed122fd
Aggrathon <antonbjo@gmail.com>
2018-08-12 16:46:39 +0300

    Tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour
+{
+	public GameObject[] levels;
+
+	private void Awake()
+	{
+		FindObjectOfType<GameProgression>().onLevelChanged.AddListener(ShowTutorial);
+	}
+
+	public void ShowTutorial(int index)
+	{
+		for (int i = 0; i < levels.Length; i++)
+			if (levels[i] != null)
+				levels[i].SetActive(false);
+		if (index < levels.Length)
+			if (levels[index] != null)
+				levels[index].SetActive(true);
+	}
+}
